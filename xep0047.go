988717d3c568dea5836f27df18ca1fa4a81aca46
Y. T. Chung <zonyitoo@gmail.com>
2014-07-11 01:20:02 +0800

    add xep0047 in-band bytestreams

---
+++ xep0047.go
@@ -0,0 +1,26 @@
+package xmpp
+
+import (
+    "encoding/xml"
+)
+
+const XMLNS_JABBER_PROTOCOL_INBAND_TYPESTREAM = "http://jabber.org/protocol/ibb"
+
+type XMPPProtocolInBandByteStreamOpen struct {
+    XMLName   xml.Name `xml:"http://jabber.org/protocol/ibb open"`
+    BlockSize uint     `xml:"block-size,attr"`
+    SID       string   `xml:"sid,attr"`
+    Stanza    string   `xml:"stanza,attr,omitempty"`
+}
+
+type XMPPProtocolInBandByteStreamData struct {
+    XMLName xml.Name `xml:"http://jabber.org/protocol/ibb data"`
+    Seq     uint     `xml:"seq,attr"`
+    SID     string   `xml:"sid,attr"`
+    Data    string   `xml:",chardata"`
+}
+
+type XMPPProtocolInBandByteStreamClose struct {
+    XMLName xml.Name `xml:"http://jabber.org/protocol/ibb close"`
+    SID     string   `xml:"sid,attr"`
+}
